2029f095a5d9d2b2b4acbc4df6a2a8b75b6116c6
Dvis <gokhaledurgesh4@gmail.com>
2024-10-31 12:59:12 +0530

    Auto Fixes

--- info.py
+++ info.py
@@ -1,3 +1,11 @@
+#
+# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+#
+# This file is part of < https://github.com/IamDvis/DV-FILE2LINK > project,
+# and is released under the MIT License.
+# Please see < https://github.com/IamDvis/DV-FILE2LINKC/blob/master/LICENSE >
+#
+# All rights reserved.
 import re
 from os import environ
 
@@ -24,13 +32,13 @@ URL = environ.get("URL", "")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7602449220').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1679112664').split()]
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "ERASTREAM")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'teraboxlinks.com')
+SHORTLINK = bool(environ.get('SHORTLINK', True)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'teraboxlinks.com/api')
 SHORTLINK_API = environ.get('SHORTLINK_API', '4b7ded045ff9b38818660f6f96995288d6ca59e2')
